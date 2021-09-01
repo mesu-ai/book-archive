898e8f4620eb65f06ff8fa4c82d115eaff2a20ac
mesu-ai <mesu.s.ss@gmail.com>
2021-09-02 05:20:19 +0600

    search result processing

--- book.js
+++ book.js
@@ -17,17 +17,18 @@ showData = datas => {
 
     const books = datas.docs.slice(0, 5);
     books.forEach(book => {
-        console.log(book);
-        console.log(...book.author_name);
 
-        console.log(...book.publisher);
+        console.log(book);
         console.log(book.title);
+        console.log(...book.author_name);
+        console.log(...book.publisher);
 
         if (book.hasOwnProperty('publish_date')) {
             console.log('yes');
             const publish = book.publish_date;
             const fp = publish.slice(-1);
             console.log(...fp);
+            return fp;
         }
 
         const authors = book.author_name;
@@ -37,6 +38,25 @@ showData = datas => {
         });
 
 
+        const searchResult = document.getElementById('data-show');
+
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+            <div class="card text-center rounded-3 shadow-lg card-design ">
+                <img class="card-img-top" src="" alt="">
+                <div class="card-body">
+
+                    <h3 class="card-title"> Book Name: ${book.title}</h3>
+                    <h4> <i>Author Name: </i> </h4>
+                    <h5> <i>Publisher Name:</i></h5>
+                    <h6 class="card-text">First Publish:</h6>
+                </div>
+            </div>
+    
+    `;
+
+        searchResult.appendChild(div);
     });
 
 
