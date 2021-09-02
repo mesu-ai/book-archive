9a5dd4ff55404fe3278178d59c609611fad53921
mesu-ai <mesu.s.ss@gmail.com>
2021-09-02 17:32:48 +0600

    overal update

--- book.js
+++ book.js
@@ -1,15 +1,41 @@
+// ststus/massage bar
+const statusUpdate = (massage, color) => {
+    const status = document.getElementById('status-bar');
+    status.style.padding = '20px';
+    status.style.color = color;
+    status.innerText = massage;
+}
+
+
 // search data load
 const search = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
 
+    // clear search box
     searchField.value = '';
 
-    const url = `https://openlibrary.org/search.json?q=${searchText}`;
+    // check the validation of search keyword
+    if (searchText === '') {
 
-    fetch(url)
-        .then(res => res.json())
-        .then(data => showData(data));
+        // update massage
+        statusUpdate('Please Enter The Search Box !', 'red')
+
+        // clear search result
+        const searchResult = document.getElementById('data-show');
+        searchResult.textContent = '';
+
+    } else {
+
+        // data load
+        const url = `https://openlibrary.org/search.json?q=${searchText}`;
+
+        fetch(url)
+            .then(res => res.json())
+            .then(data => showData(data));
+
+    }
+
 
 }
 
@@ -17,33 +43,23 @@ const search = () => {
 // show search result
 showData = datas => {
 
-    // console.log(datas);
-    // console.log(datas.numFound);
-    const status = document.getElementById('status-bar');
-    status.style.padding = '20px';
-    status.innerText = `Total Result Found: ${datas.numFound}`;
+    statusUpdate(`Total Books Found: ${datas.numFound}`, 'green');
 
     const searchResult = document.getElementById('data-show');
     searchResult.textContent = '';
 
-    const books = datas.docs.slice(0, 10);
+    // take 1st 20 search result
+    const books = datas.docs.slice(0, 20);
     books.forEach(book => {
 
-        // console.log(book);
-        // console.log('cover:', book.cover_i);
-        // console.log('name:', book.title);
-        // console.log('author:', ...book.author_name);
-        // console.log('publisher:', ...book.publisher);
-
         const coverUrl = `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`;
+
 
         // const firstPublish = () => {
         //     if (book.hasOwnProperty('publish_date')) {
         //         const dates = book.publish_date;
-
         //         const date = dates[dates.length - 1];
         //         return date;
-
         //         // return date.slice(-1);
         //     }
         // }
@@ -51,8 +67,7 @@ showData = datas => {
 
         document.getElementById('search-container').style.padding = '25px'
 
-
-
+        // create a div and show search result
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
